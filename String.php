a35201be9530fdd9a940d040f67c0bfae2f8bbf6
bureX <knenad@gmail.com>
2015-03-02 15:33:16 +0100

    Update String.php
    Update String.php

--- String.php
+++ String.php
@@ -20,7 +20,7 @@
  *     * Neither the name of the Hoa nor the names of its contributors may be
  *       used to endorse or promote products derived from this software without
  *       specific prior written permission.
- *p
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
