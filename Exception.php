1335d3d95007fd95ce33e75bb97a8b665c86dc79
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2015-11-20 13:19:42 +0100

    Exception: Use `Hoa\Exception`.
    The core is being splitted into several libraries. `Hoa\Exception` is
    one of them.

--- Exception.php
+++ Exception.php
@@ -36,16 +36,16 @@
 
 namespace Hoa\Ustring;
 
-use Hoa\Core;
+use Hoa\Exception as HoaException;
 
 /**
  * Class \Hoa\Ustring\Exception.
  *
- * Extending the \Hoa\Core\Exception class.
+ * Extending the \Hoa\Exception\Exception class.
  *
  * @copyright  Copyright © 2007-2015 Hoa community
  * @license    New BSD License
  */
-class Exception extends Core\Exception
+class Exception extends HoaException
 {
 }
